bcce61c6361fc32bc1be663f0c76cbd5965ea6a1
James Couball <jcouball@yahoo.com>
2026-07-02 12:41:29 -0700

    fix: use YARD::Logger::ERROR instead of Logger::ERROR
    Ruby 4.0 no longer auto-requires 'logger' from stdlib.
    YARD::Logger::ERROR is the correct constant for YARD's parse method.

--- lib/yardstick/parser.rb
+++ lib/yardstick/parser.rb
@@ -10,7 +10,7 @@ module Yardstick
     #
     # @api private
     def self.parse_paths(paths)
-      YARD.parse(paths, [], Logger::ERROR)
+      YARD.parse(paths, [], YARD::Logger::ERROR)
       documents
     end
 
